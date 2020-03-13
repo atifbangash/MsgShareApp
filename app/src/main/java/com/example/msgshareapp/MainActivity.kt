8c6146fcb5539bcccb6163c4769247ec62921207
atifbangash@gmail.com <AmAfHh+786>
2020-03-13 10:36:03 -0400

    ToastMessageScript

---
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.msgshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        BtnShowToast.setOnClickListener {
+            Log.i("MainActivity", "button was clicked")
+            Toast.makeText(this,"Button was clicked", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Clicked for sure",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Yup",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Freaky",Toast.LENGTH_LONG).show()
+        }
+    }
+}
