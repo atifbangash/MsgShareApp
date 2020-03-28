de139d9c7f2243a6958a599cdf8f9847feb3196c
atifbangash@gmail.com <AmAfHh+786>
2020-03-28 10:15:25 -0400

    Second screen video 3

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,9 +15,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         BtnShowToast.setOnClickListener {
             Log.i("MainActivity", "button was clicked")
-            Toast.makeText(this,"Button was clicked", Toast.LENGTH_SHORT).show()
+           Toast.makeText(this,"Button was clicked", Toast.LENGTH_SHORT).show()
             Toast.makeText(this, "Clicked for sure",Toast.LENGTH_LONG).show()
-            Toast.makeText(this, "Yup",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Yup",Toast.LENGTH_SHORT).show()
         }
+        btnSendMsgToNextActivity.setOnClickListener {
+            val  message:String = etUserMessage.text.toString()
+            Toast.makeText(this, message,Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+startActivity(intent)
+        }
+
     }
 }
